cff6218016364cec671e669c11ecca3fe84802bd
LAPTOP-V7IF14BU\jarom <jaro92@hotmail.com>
2022-05-10 12:40:01 +0200

    update for new motor

--- klankschaal_v2/klankschaal_v2.ino
+++ klankschaal_v2/klankschaal_v2.ino
@@ -1,17 +1,19 @@
 // Define pin connections & motor's steps per revolution
-const int dirPin = 2;
-const int stepPin = 3;
+const int dirPin = 6;
+const int stepPin = 5;
 const int hallPin = A0;
 const int stepsPerRevolution = 200;
 
+
+const int startMotorSpeed = 950;
 const int minMotorSpeed = 750;
-const int maxMotorSpeed = 125;
-const int speedStep = 25;
+const int maxMotorSpeed = 300;
 
 int stepCount = 0;
 int hallCount = 0;
 
 float motorAccum = 0.0;
+float startAccum = 0.0;
 
 int motorSpeed = minMotorSpeed;
 
@@ -27,6 +29,8 @@ int stepTreshold = 0;
 
 unsigned long delta = 0;
 
+bool speedSwitch = 0;
+
 void setup()
 {
   // Declare pins as Outputs
@@ -41,21 +45,30 @@ void setup()
 }
 void loop()
 {
+
+  if(millis() < 100){
+      previousTime = millis();
+      stopDelayTime = millis();  
+      prevSensorTime=millis();  
+  }
+
   
   digitalWrite(13,HIGH);
   
   int hallSensor = analogRead(A0);
 
   //if hallSensor senses magnet
-  if(hallSensor < 1000){
+  if(hallSensor < 500){
+
+
     
     if(stepSwitch == 0){  //switch on only once
       hallCount++;
-      Serial.println(hallCount);
+      Serial.println("hallcount: " + (String)hallCount);
       
       stepCount++;
       stepCount = min(2,stepCount);
-      motorSpeed = maxMotorSpeed;
+      motorSpeed = startMotorSpeed;
       previousTime = millis();
       stopDelayTime = millis();
 
@@ -71,29 +84,55 @@ void loop()
     stepSwitch = 0;
   }
 
+  currentTime = millis();
+
+
   if(hallCount > 1){//if two sensors have passed
 
-    currentTime = millis();
-
     moveMotor();
+     
+    if(speedSwitch == 0){ //ramp speed up
+      startAccum+=1;
+      motorSpeed = startMotorSpeed - (int)startAccum;
 
-    //damping speed
-    motorAccum+=0.02;
-    
-    motorSpeed = maxMotorSpeed + (int)motorAccum;
-    motorSpeed = constrain(motorSpeed,maxMotorSpeed,minMotorSpeed); //clip motor speed
+      if(motorSpeed <= maxMotorSpeed){ //if maximum speed is reached
+        speedSwitch = 1;
+      }
+
+      
+    }else{ //ramp speed down
+
+      //damping speed
+      motorAccum+=0.05;//0.025;
+      
+      motorSpeed = maxMotorSpeed + (int)motorAccum;
+      motorSpeed = constrain(motorSpeed,maxMotorSpeed,minMotorSpeed); //clip motor speed
+
+    }
     
     //wait before counting back
     if(currentTime - stopDelayTime > 2000){
       countBack();
     }//if
+
     
-  }else{//reset
+  }else if (hallCount == 1){
+
+    motorSpeed = startMotorSpeed;
+    moveMotor();
+
+    //wait before counting back
+    if(currentTime - stopDelayTime > 2000){
+      countBack();
+    }//if
+  
+  
+  }else {//reset
     stopDelayTime = currentTime;
     motorAccum=0.0;
+    speedSwitch = 0;
   }
   
-  //Serial.println(stepCount);
 
 }
 
@@ -102,18 +141,21 @@ void moveMotor(){
   delayMicroseconds(motorSpeed);
   digitalWrite(stepPin, LOW);
   delayMicroseconds(motorSpeed);
+  
+  //Serial.println(motorSpeed);
 }
 
 
 void countBack(){
  
-  if(currentTime - previousTime > 500){
-    motorAccum+=0.05; //extra damping after delay
+  if(currentTime - previousTime > 1000){
+    motorAccum+=0.015; //extra damping after delay
 
     stepCount--;
     stepCount = max(0,stepCount);
     if(stepCount == 0){
       hallCount = 0;
+      startAccum = 0;
     }
 
     //Serial.println(stepCount);
